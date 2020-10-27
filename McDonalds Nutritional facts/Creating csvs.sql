dde126bf97b9810de4093e566b34102d66d44847
JosiahM42 <otisjames42@outlook.com>
2020-10-27 23:22:02 +0000

    huge change

--- McDonalds Nutritional facts/Creating csvs.sql
+++ McDonalds Nutritional facts/Creating csvs.sql
@@ -1,7 +1,8 @@
 SELECT *
  FROM mcdonalds.menu
  -- WHERE catagory = 'Breakfast'
- -- WHERE catagory = 'Beef & Pork' OR catagory = 'Chicken & Fish'
- WHERE catagory = 'Coffee & Tea' OR catagory = 'Smoothies & Shakes' 
- OR catagory = 'Beverages'
+ -- WHERE catagory = 'Beef & Pork' OR catagory = 'Chicken & Fish' OR catagory = 'Salads'
+ -- WHERE catagory = 'Coffee & Tea' OR catagory = 'Smoothies & Shakes' 
+ -- OR catagory = 'Beverages'
+ WHERE catagory = 'Snacks & Sides' OR catagory = 'Desserts'
 ;
